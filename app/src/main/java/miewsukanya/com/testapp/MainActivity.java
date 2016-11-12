32b0aef07cb8eb2105d705fd9445167ff39360bb
miewsukanya <miewsukanya.24@gmail.com>
2016-11-12 14:49:17 +0700

    Create Finish

--- app/src/main/java/miewsukanya/com/testapp/MainActivity.java
+++ app/src/main/java/miewsukanya/com/testapp/MainActivity.java
@@ -1,13 +1,35 @@
 package miewsukanya.com.testapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget ผูกกับตัวแปร
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2); // ; Ctrl+Shift+Enter
+
+        //Sign up Controller
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+
+//
+    } //Main Method
+} //Main class คลาสหลัก
