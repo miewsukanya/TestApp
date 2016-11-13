fdb704b49b8a2e4834ca9a055a86d1b5e801e4ea
miewsukanya <miewsukanya.24@gmail.com>
2016-11-13 15:21:11 +0700

    Finish project

---
+++ app/src/main/java/miewsukanya/com/testapp/SplashScreen.java
@@ -0,0 +1,31 @@
+package miewsukanya.com.testapp;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        //Sound Effect
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        mediaPlayer.start();
+        //auto intent
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },9000);
+    }//Main method
+
+}//Main Class
