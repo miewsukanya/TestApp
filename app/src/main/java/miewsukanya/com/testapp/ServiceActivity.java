f936d5ec30913e21b775ac8ab74b761f64cc172b
miewsukanya <miewsukanya.24@gmail.com>
2016-11-13 14:58:28 +0700

    call finish

--- app/src/main/java/miewsukanya/com/testapp/ServiceActivity.java
+++ app/src/main/java/miewsukanya/com/testapp/ServiceActivity.java
@@ -1,5 +1,12 @@
 package miewsukanya.com.testapp;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,36 +14,76 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ServiceActivity extends AppCompatActivity {
+
     //Explicit
     private ListView listView;
-    private  String[] nameStrings,phoneStrings, imageStrings;
+    private String[] nameStrings, phoneStrings, imageStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-        //Bind widget
+
+        //Bind Widget
         listView = (ListView) findViewById(R.id.livFriend);
 
-        //Receive value from Intent รับค่ามาจาก mainactivity
+        //Receive Value From Intent
         nameStrings = getIntent().getStringArrayExtra("Name");
         phoneStrings = getIntent().getStringArrayExtra("Phone");
         imageStrings = getIntent().getStringArrayExtra("Image");
-        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings, phoneStrings, imageStrings);
+
+        //Create ListView
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings,
+                phoneStrings, imageStrings);
         listView.setAdapter(myAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                confirmCall(nameStrings[position], phoneStrings[position]);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-            }//onItemClick
+                confirmCall(nameStrings[i], phoneStrings[i]);
+
+            }   // onItemClick
         });
 
-    }//Main Method
 
-    private void confirmCall(String nameString, String phoneString) {
-        
-    }
+    }   // Main Method
 
-}//Main Class
+    private void confirmCall(String nameString, final String phoneString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ServiceActivity.this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("Please Confirm");
+        builder.setMessage("คุณต้องการโทรหา " + nameString + " จริงๆ หรือ ?");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:=" + phoneString));
+                if (ActivityCompat.checkSelfPermission(ServiceActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+
+            }
+        });
+        builder.show();
+
+    }   // confirm
+
+}      // Main Class
