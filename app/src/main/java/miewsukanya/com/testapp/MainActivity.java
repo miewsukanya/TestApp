35c5bee5ef14a6623f9248f30eaf76f26828bcc3
miewsukanya <miewsukanya.24@gmail.com>
2016-11-13 13:10:53 +0700

    Sign Alert

--- app/src/main/java/miewsukanya/com/testapp/MainActivity.java
+++ app/src/main/java/miewsukanya/com/testapp/MainActivity.java
@@ -1,25 +1,66 @@
 package miewsukanya.com.testapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private Button signInButton, signUpButton;
-
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+    private MyConstant myConstant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myConstant = new MyConstant();
+
         //Bind Widget ผูกกับตัวแปร
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2); // ; Ctrl+Shift+Enter
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
+        //SignIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get Value from Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //ceck space
+                if (userString.equals("")|| passwordString.equals("")) {
+                    //HaveSpace
+                    MyAlert myAlert = new MyAlert(MainActivity.this, R.drawable.bird48,
+                            getResources().getString(R.string.title_HaveSpace),
+                            getResources().getString(R.string.message_HaveSpace));
+                    myAlert.MyDialog();
+                } else {
+                    //NoSpace
+                    SynUser synUser = new SynUser(MainActivity.this);
+                    synUser.execute(myConstant.getUrlGetJSON());
+                }
+            }//onClick
+        });
 
         //Sign up Controller
 
@@ -32,4 +73,85 @@ public class MainActivity extends AppCompatActivity {
 
 //
     } //Main Method
+
+    private class SynUser extends AsyncTask<String, Void, String> {
+        //Explicit
+        private Context context;
+        private String[] nameStrings,phoneStrings, imageStrings;
+        private String truePassword;
+        private Boolean aBoolean = true;
+
+        public SynUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(params[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("13novV2", "e doIn==>" + e.toString());
+                return null;
+            }
+           //return null;
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d("13novV2", "Json ==>" + s);
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+
+                nameStrings = new String[jsonArray.length()];
+                phoneStrings = new String[jsonArray.length()];
+                imageStrings = new String[jsonArray.length()];
+                for (int i=0;i<jsonArray.length();i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    nameStrings[i] = jsonObject.getString("Name");
+                    phoneStrings[i] = jsonObject.getString("Phone");
+                    imageStrings[i] = jsonObject.getString("Image");
+
+                    Log.d("13novV3", "name(" + i + ") ==>" + nameStrings);
+                    //chsck user
+                    if (userString.equals(jsonObject.getString("User"))) {
+                        aBoolean = false;
+                        truePassword = jsonObject.getString("Password");
+
+                    }
+
+                }//for
+                if (aBoolean) {
+                    //user False
+                    MyAlert myAlert = new MyAlert(context, R.drawable.kon48,
+                            getResources().getString(R.string.title_UserFalse),
+                            getResources().getString(R.string.message_UserFalse));
+                    myAlert.MyDialog();
+
+                } else if (phoneStrings.equals(truePassword)) {
+                    //PW True
+                    Toast.makeText(context,"Welcome",Toast.LENGTH_LONG).show();
+                } else {
+                    //PW False
+                    MyAlert myAlert = new MyAlert(context, R.drawable.kon48,
+                            getResources().getString(R.string.title_PassFalse),
+                            getResources().getString(R.string.message_PassFalse));
+                    myAlert.MyDialog();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //super.onPostExecute(s);
+
+        }//onPost
+
+    }//SnyUser
+
+
 } //Main class คลาสหลัก
--- app/src/main/java/miewsukanya/com/testapp/SignUpActivity.java
+++ app/src/main/java/miewsukanya/com/testapp/SignUpActivity.java
@@ -1,10 +1,12 @@
 package miewsukanya.com.testapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
@@ -14,6 +16,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import org.jibble.simpleftp.SimpleFTP;
 
@@ -22,24 +31,26 @@ import java.io.File;
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
-    //เติม Type Ctrl+ Space
-    private EditText nameEditText,phoneEditText,usernameEditText, passwordEditText;
+    private EditText nameEditText, phoneEditText,
+            userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString,phoneString,userString,passwordString,imagePathString,imageNameString;
+    private String nameString, phoneString, userString, passwordString,
+            imagePathString, imageNameString;
     private Uri uri;
-    private Boolean aBoolean = true;
+    private boolean aBoolean = true;
+    private String urlAddUser = "http://swiftcodingthai.com/kku/add_user_master.php";
+    private String urlImage = "http://swiftcodingthai.com/kku/Image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //Bind widget
-        //key == alt+enter = cath to,alt+ctrl+enter = ; ปิด ;
+        //Bind Widget
         nameEditText = (EditText) findViewById(R.id.editText);
         phoneEditText = (EditText) findViewById(R.id.editText2);
-        usernameEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText3);
         passwordEditText = (EditText) findViewById(R.id.editText4);
         imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button3);
@@ -48,101 +59,187 @@ public class SignUpActivity extends AppCompatActivity {
         //SignUp Controller
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+
                 //Get Value From Edit Text
-                nameString = nameEditText.getText().toString().trim(); //ถ้ากด SignUp แล้วมีช่องว่างจะตัดออก
+                nameString = nameEditText.getText().toString().trim();
                 phoneString = phoneEditText.getText().toString().trim();
-                userString = usernameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
                 passwordString = passwordEditText.getText().toString().trim();
 
-                //check Space
-                if (nameString.equals("") || phoneString.equals("")||
-                        userString.equals("")|| passwordString.equals("")) {
-                    //Have space check log
+                //Check Space
+                if (nameString.equals("") || phoneString.equals("") ||
+                        userString.equals("") || passwordString.equals("")) {
+                    //Have Space
                     Log.d("12novV1", "Have Space");
-                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48, "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องครับ");
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48,
+                            "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องคะ");
                     myAlert.MyDialog();
                 } else if (aBoolean) {
-                    //Non choose Image
-                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48, "ยังไม่เลือกรูป", "กรุณาเลือกรูปด้วยครับ");
+                    //Non Choose Image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48,
+                            "ยังไม่เลือกรูป", "กรุณาเลือกรูปด้วยคะ");
                     myAlert.MyDialog();
                 } else {
                     //Choose Image OK
                     upLoadImageToServer();
+                    upLoadStringToServer();
+
                 }
 
-            }//on click
+
+            }   // onClick
         });
 
 
-         //Image Controller
+        // Image Controller
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //intent program
-                intent.setType("image/*"); // select photo เปิดโปรแกรมไหนที่มีภาพ
-                startActivityForResult(Intent.createChooser(intent,"โปรดเลือกแอปดูภาพ"),0); //ใส่ int อะไรก็ได้
+            public void onClick(View view) {
 
-            }//on click
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรเลือกแอฟดูภาพ"), 0);
+
+            }   // onClick
         });
-    }//Main Method
+
+
+    }   // Main Method
+
+    private void upLoadStringToServer() {
+
+        AddNewUser addNewUser = new AddNewUser(SignUpActivity.this);
+        addNewUser.execute(urlAddUser);
+
+
+    }   // upLoad
+
+    //Create Inner Class
+    private class AddNewUser extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+
+        public AddNewUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("Name", nameString)
+                        .add("Phone", phoneString)
+                        .add("User", userString)
+                        .add("Password", passwordString)
+                        .add("Image", urlImage + imageNameString)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("13novV1", "e doIn ==> " + e.toString());
+
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("13novV1", "Result ==> " + s);
+            if (Boolean.parseBoolean(s)) {
+                Toast.makeText(context,"Upload Success",Toast.LENGTH_LONG).show();
+                finish();
+            } else {
+                Toast.makeText(context,"Cannot Success",Toast.LENGTH_LONG).show();
+            }
+
+        }   // onPost
+
+    }   // AddNewUser Class
+
 
     private void upLoadImageToServer() {
-        //check Policy
-        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        //Change Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
         StrictMode.setThreadPolicy(threadPolicy);
+
         try {
+
             SimpleFTP simpleFTP = new SimpleFTP();
-            simpleFTP.connect("ftp.swiftcodingthai.com",21,"kku@swiftcodingthai.com","Abc12345");
+            simpleFTP.connect("ftp.swiftcodingthai.com", 21,
+                    "kku@swiftcodingthai.com", "Abc12345");
             simpleFTP.bin();
             simpleFTP.cwd("Image");
             simpleFTP.stor(new File(imagePathString));
             simpleFTP.disconnect();
 
         } catch (Exception e) {
-            Log.d("12novV1", "e simpleFTP ==>" + e.toString());
+            Log.d("12novV1", "e simpleFTP ==> " + e.toString());
         }
 
 
-    }//upLoad
+
+    }   // upLoad
 
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
                                     Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if ((requestCode == 0)&&(resultCode == RESULT_OK)) {
+
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
 
             Log.d("12novV1", "Result OK");
             aBoolean = false;
-            //show Image
+
+            //Show Image
             uri = data.getData();
             try {
+
                 Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
                         .openInputStream(uri));
                 imageView.setImageBitmap(bitmap);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            //Find path of image
+
+            //Find Path of Image
             imagePathString = myFindPath(uri);
-            Log.d("12novV1", "imagePath ==>" + imagePathString);
-            //find name of Image
+            Log.d("12novV1", "imagePath ==> " + imagePathString);
+
+            //Find Name of Image
             imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
-            Log.d("12novV1", "imageName ==>" + imageNameString);
-
-        }//if
+            Log.d("12novV1", "imageName ==> " + imageNameString);
 
 
-    }//onActivityResult
+
+        }   // if
+
+    }   // onActivity
 
     private String myFindPath(Uri uri) {
 
         String result = null;
         String[] strings = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri, strings, null, null,null);
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
 
         if (cursor != null) {
+
             cursor.moveToFirst();
             int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             result = cursor.getString(index);
@@ -150,6 +247,9 @@ public class SignUpActivity extends AppCompatActivity {
         } else {
             result = uri.getPath();
         }
+
+
         return result;
     }
-}//Main Class
+
+}   // Main Class
